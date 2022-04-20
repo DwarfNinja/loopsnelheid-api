6e79a1ab70e9ad45b8d702a561d7144611ec6bc3
Yassine el Fahmi <contact@yassinefahmi.nl>
2022-04-20 16:59:29 +0200

    Feat: Added User and Sex to domain account

---
+++ src/main/java/nl/app/loopsnelheid/account/domain/User.java
@@ -0,0 +1,78 @@
+package nl.app.loopsnelheid.account.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+
+@Entity
+@Table(name = "users",
+        indexes = {
+                @Index(name = "unique_email_index", columnList = "email", unique = true),
+        })
+@Getter
+@Builder
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private Date dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    private Sex sex;
+
+    public User() {
+
+    }
+
+    public User(Long id, String email, String password, Date dateOfBirth, Sex sex) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.sex = sex;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
