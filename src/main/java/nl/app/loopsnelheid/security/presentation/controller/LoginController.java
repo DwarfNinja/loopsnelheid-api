06006a9d7edaf8c47f582690d5bc4249d0b009df
Yassine el Fahmi <contact@yassinefahmi.nl>
2022-05-25 04:06:11 +0200

    Feat: authenticate user

---
+++ src/main/java/nl/app/loopsnelheid/security/presentation/controller/LoginController.java
@@ -0,0 +1,28 @@
+package nl.app.loopsnelheid.security.presentation.controller;
+
+import lombok.RequiredArgsConstructor;
+import nl.app.loopsnelheid.security.application.LoginService;
+import nl.app.loopsnelheid.security.domain.JwtResponse;
+import nl.app.loopsnelheid.security.presentation.dto.JwtResponseDto;
+import nl.app.loopsnelheid.security.presentation.dto.LoginDto;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class LoginController {
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public JwtResponseDto authenticateUser(@Valid @RequestBody LoginDto loginDto) {
+        JwtResponse jwtResponse = loginService.authenticateUser(loginDto.email, loginDto.password);
+
+        return new JwtResponseDto(jwtResponse.getJwt(), jwtResponse.getEmail(), jwtResponse.getRoles());
+    }
+}
--- src/main/java/nl/app/loopsnelheid/security/presentation/dto/UserProfileDto.java
+++ src/main/java/nl/app/loopsnelheid/security/presentation/dto/UserProfileDto.java
@@ -5,6 +5,6 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class UserProfile {
+public class UserProfileDto {
     private final String email;
 }
